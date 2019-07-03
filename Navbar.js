3f8209ce293ec683224cc8cc0fc332023be960d4
Meet Panchani <48985892+MeetPanchani@users.noreply.github.com>
2019-07-03 18:26:55 +0530

    navbar updated
    change in navbar list names

--- Navbar.js
+++ Navbar.js
@@ -7,9 +7,9 @@ const Navbar = () => {
             <div className="menuitem">
                 <ul>
                     <li><Link to="/bear_spread_call">Bear Spread using call</Link></li>
-                    <li><Link to="/bear_spread_put">Bear Spread put</Link></li>
+                    <li><Link to="/bear_spread_put">Bear Spread usng put</Link></li>
                     <li><Link to="/bear_spread_call">Bull Spread using call</Link></li>
-                    <li><Link to="/bull_spread_put">Bull Spread put</Link></li>
+                    <li><Link to="/bull_spread_put">Bull Spread using put</Link></li>
                     <li><Link to="/butterfly_spread">Butterfly Spread</Link></li>
                 </ul>
             </div>
